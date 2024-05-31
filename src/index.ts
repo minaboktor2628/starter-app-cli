c569153d279953a7a54f74cf7263eb963c4dcab1
minaboktor <mina26b8@gmail.com>
2024-05-30 21:57:36 -0400

    ts conf + initial screen

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,66 @@
-export function sayhello(){
-// 	...
-	console.log("hello");
+#!/usr/bin/env node
+import welcome from "cli-welcome";
+// @ts-ignore
+import pkg from "./../package.json";
+import figlet from "figlet";
+import gradient from "gradient-string";
+import inquirer from "inquirer";
+import path from "path";
+import { createSpinner } from "nanospinner";
+import chalk from "chalk";
+import simpleGit, { SimpleGit, SimpleGitOptions } from "simple-git";
+
+const sleep = (ms = 100) => new Promise((r) => setTimeout(r, ms));
+let isArgIncludeDot = false;
+
+async function init() {
+  welcome({
+    title: "Next.js starter app",
+    tagLine: `by ${pkg.author}`,
+    description: pkg.description,
+    version: pkg.version,
+    bgColor: "#204216",
+    color: "#000000",
+    bold: true,
+  });
+  figlet("Mina ' s   Boilerplate", (err, data) => {
+    console.log(gradient.pastel.multiline(data));
+  });
+  await sleep();
 }
+async function askProjectName() {
+  const spinner = createSpinner(`Checking input...`).start();
+  await sleep(500);
+
+  // Check if the last argument is "."
+  const lastArg = process.argv[process.argv.length - 1];
+  const isLastArgDot = lastArg === ".";
+
+  if (isLastArgDot) {
+    isArgIncludeDot = true;
+    const currentDir = process.cwd();
+    const dirName = path.basename(currentDir);
+    spinner.success({
+      text: `Using current directory's name as project name: ${chalk.bold.bgGreen(dirName)}`,
+    });
+    return dirName;
+  } else {
+    spinner.stop();
+    spinner.clear();
+    const answer = await inquirer.prompt({
+      name: "project_name",
+      type: "input",
+      message: "Whats your projects name?\n",
+      default: path.basename(process.cwd()),
+    });
+    return answer.project_name;
+  }
+}
+
+async function main() {
+  await init();
+
+  await askProjectName();
+}
+
+main().then(() => process.exit(0));
